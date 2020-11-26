cb627ede346a5c810e67f7f1e848b0ff2c4ce651
chenxiangyu <850586765@qq.com>
2020-11-26 17:36:51 +0800

    jenkins自动化部署测试

---
+++ src/main/java/com/cxy/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.cxy.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ：陈翔宇
+ * @date ：2020/11/26 17:35
+ * @description：
+ * @modified By：
+ * @version: $
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
