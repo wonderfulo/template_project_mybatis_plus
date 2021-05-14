ac6df23cd200dc518d88d949418a2eeccbc3a9cf
chenxiangyu <850586765@qq.com>
2021-05-14 16:10:16 +0800

    logback-spring.xml  改造

--- src/main/java/com/cxy/controller/TestController.java
+++ src/main/java/com/cxy/controller/TestController.java
@@ -2,6 +2,8 @@ package com.cxy.controller;
 
 import com.cxy.assert_package.AssertEx;
 import com.cxy.constant.exception.DisplayTaskEnumException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
 
+    private static final Logger logger  = LoggerFactory.getLogger(TestController.class);
+
     @RequestMapping("/hello")
     public String hello(){
         return "hello";
@@ -28,6 +32,11 @@ public class TestController {
 
     @RequestMapping("")
     public String test(){
+        logger.info("------------info--------------{}",123);
+        logger.error("------------error--------------{}",123);
+        logger.debug("------------debug--------------{}",123);
+        logger.trace("------------trace--------------{}",123);
+        logger.warn("------------warn--------------{}",123);
         AssertEx.isNotEmpty(0L, DisplayTaskEnumException.ID_NOT_EXIST);
         return "success";
     }
--- src/main/java/com/cxy/interceptor/GlobalInterceptor.java
+++ src/main/java/com/cxy/interceptor/GlobalInterceptor.java
@@ -3,7 +3,9 @@ package com.cxy.interceptor;
 import com.cxy.assert_package.AssertEx;
 import com.cxy.constant.exception.MyEnumException;
 import com.cxy.exception.MyException;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
 import org.springframework.lang.Nullable;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -17,25 +19,30 @@ import javax.servlet.http.HttpServletResponse;
  * @author: 陈翔宇
  * @create_time: 2020-11-15 12:21:44
  */
+@Slf4j
 public class GlobalInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
         System.out.println("处理器前方法");
+        String accessToken = request.getParameter("accessToken");
+        if (StringUtils.isNotBlank(accessToken)) {
+            log.debug("LoggingWebSocketServer 任务开始");
+        }
         AssertEx.isNotEmpty(request.getParameter("accessToken"), MyEnumException.ACCESS_TOKEN_NO_EXIST);
         return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-                            @Nullable ModelAndView modelAndView) throws Exception {
+                           @Nullable ModelAndView modelAndView) throws Exception {
 //        System.out.println("处理器后方法");
     }
 
     @Override
-    public  void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
-                                 @Nullable Exception ex) throws Exception {
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
+                                @Nullable Exception ex) throws Exception {
 //        System.out.println("处理器完成方法");
     }
 }
