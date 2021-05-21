4cbe84f1b59623e33920916ba010436576646058
chenxiangyu <850586765@qq.com>
2021-05-21 16:21:42 +0800

    @Scheduled test

--- src/main/java/com/cxy/schedule/ScheduleTest.java
+++ src/main/java/com/cxy/schedule/ScheduleTest.java
@@ -1,5 +1,7 @@
 package com.cxy.schedule;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -12,6 +14,9 @@ import org.springframework.stereotype.Component;
  */
 //@Component
 public class ScheduleTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(ScheduleTest.class);
+
     //计数器
     int count1 = 1;
     int count2 = 1;
@@ -29,5 +34,20 @@ public class ScheduleTest {
     @Async
     public void job2(){
         System.out.println("线程：" + Thread.currentThread().getName() + ",job2: 每秒执行一次，执行第【" + count2++ + "】次");
+    }
+
+
+
+    // 每秒执行一次
+//    @Scheduled(cron = "* * * * * ?", zone = "Asia/Shanghai")
+    @Scheduled(fixedRate = 1000)
+    @Async
+    public void logTest(){
+
+        logger.info("------------info--------------{}",123);
+        logger.error("------------error--------------{}",123);
+        logger.debug("------------debug--------------{}",123);
+        logger.trace("------------trace--------------{}",123);
+        logger.warn("------------warn--------------{}",123);
     }
 }
