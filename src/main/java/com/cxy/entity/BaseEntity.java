0436ade194aa71c87a376b67a0db8027e49913f2
chenxiangyu <850586765@qq.com>
2021-07-05 15:58:03 +0800

    实体类追加市时区

--- src/main/java/com/cxy/entity/BaseEntity.java
+++ src/main/java/com/cxy/entity/BaseEntity.java
@@ -38,7 +38,7 @@ public class BaseEntity implements Serializable {
      * * @JsonFormat: 返回数据时，格式转换
      * * @DateTimeFormat: 请求数据时，格式转换
      */
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", locale = "zh",timezone = "GMT+8")
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date createTime;
 
@@ -52,7 +52,7 @@ public class BaseEntity implements Serializable {
      * * @JsonFormat: 返回数据时，格式转换
      * * @DateTimeFormat: 请求数据时，格式转换
      */
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", locale = "zh",timezone = "GMT+8")
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date updateTime;
 
