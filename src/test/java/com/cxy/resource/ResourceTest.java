477ff265619960606648a13aae6ba509316b872f
chenxiangyu <850586765@qq.com>
2021-07-08 17:51:56 +0800

    spring Resource 测试

---
+++ src/test/java/com/cxy/resource/ResourceTest.java
@@ -0,0 +1,45 @@
+package com.cxy.resource;
+
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.UrlResource;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ResourceTest {
+    public static void main(String[] args) throws IOException {
+        ResourceLoader resloLoader = new DefaultResourceLoader();
+        Resource res = resloLoader.getResource("https://www.baidu.com/");
+        System.out.println(res instanceof UrlResource); // true
+        BufferedReader bf = new BufferedReader(new InputStreamReader(res.getInputStream()));
+        StringBuilder sb = new StringBuilder();
+        String temp = null;
+        while ((temp = bf.readLine())!= null) {
+            sb.append(temp);
+        }
+        System.out.println(sb.toString());
+
+        System.out.println("\n-----------------------------\n");
+        res = resloLoader.getResource("classpath:test.txt");
+        bf = new BufferedReader(new InputStreamReader(res.getInputStream()));
+        sb = new StringBuilder();
+        temp = null;
+        while ((temp = bf.readLine())!= null) {
+            sb.append(temp);
+        }
+        System.out.println(sb.toString());
+
+        System.out.println("\n-----------------------------\n");
+        res = resloLoader.getResource("file:C:\\Users\\ricco\\Desktop\\test\\test.txt");
+        bf = new BufferedReader(new InputStreamReader(res.getInputStream()));
+        sb = new StringBuilder();
+        temp = null;
+        while ((temp = bf.readLine())!= null) {
+            sb.append(temp);
+        }
+        System.out.println(sb.toString());
+    }
+}
